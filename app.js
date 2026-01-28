27e06b5c2734604c1d7b2f928db3ff8bc4496394
wizardabhi77 <vemulapalliabhinav@gmail.com>
2026-01-28 18:10:55 +0530

    create index.ejs view
    display messages in a list

--- app.js
+++ app.js
@@ -1,9 +1,16 @@
 
 const express = require("express");
+const path = require("node:path");
 
 const app = express();
+const indexRouter = require("./routes/indexRouter");
+const newRouter = require("./routes/newRouter");
 
-app.get("/", (req, res) => res.send("HELOOOOOO"));
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs")
+
+app.use("/", indexRouter);
+app.use("/new",newRouter);
 
 const PORT = 3000;
 app.listen(PORT, (error) => {
---
+++ routes/indexRouter.js
@@ -0,0 +1,23 @@
+
+const { Router } = require("express");
+
+const indexRouter = Router();
+
+const messages = [
+    {
+    text: "Hi there!",
+    user: "Amando",
+    added: new Date()
+  },
+  {
+    text: "Hello World!",
+    user: "Charles",
+    added: new Date()
+  }
+];
+
+indexRouter.get("/", (req, res) => {
+    res.render("index", {'messages': messages});
+});
+
+module.exports = indexRouter;
